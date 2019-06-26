2f34bd0c19b7fd00e05826835cc24365be18c6ea
das24081999 <solrevisdor143@gmail.com>
2019-06-25 19:51:45 -0500

    primer esquema

---
+++ proyecto_final/gotitas_del_saber/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GotitasDelSaberConfig(AppConfig):
+    name = 'gotitas_del_saber'
---
+++ proyecto_final/gotitas_del_saber/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import  url
+from django.urls import include, path
+
+from .views import *
+from django.contrib.auth import views as auth_views
+from django.views.decorators.csrf import csrf_exempt
+
+
+urlpatterns=[
+
+
+]
